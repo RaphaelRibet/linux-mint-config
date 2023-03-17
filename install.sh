adcd210b55040157320afbe6362535e7d667fb4e
RaphaelRibet <40607556+RaphaelRibet@users.noreply.github.com>
2023-03-17 22:53:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-# Install Languages
-sudo apt install build-essential
+# ESSENTIALS
+sudo apt install build-essential git cmake
+
+## Node & NPM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install --lts
+source ~/.bashrc
+
+## Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.bashrc
+cargo --version
+
+## HACK FONT
+sudo apt install fonts-hack-ttf -y
+fc-cache -f -v
+mkdir Hack
+cd Hack
+wget https://raw.githubusercontent.com/Shadient/config-mint/main/fonts/hack-fonts.txt
+wget -i hack-fonts.txt
+rm hack-fonts.txt
+cd ~
