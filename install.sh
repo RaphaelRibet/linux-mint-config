08050e8583cb089d3230062ae029fd3a1e8571b3
RaphaelRibet <40607556+RaphaelRibet@users.noreply.github.com>
2023-03-17 23:10:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ESSENTIALS
-sudo apt install build-essential git cmake
+sudo apt install ripgrep build-essential git cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev -y
 
 ## Node & NPM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
@@ -23,3 +23,40 @@ wget https://raw.githubusercontent.com/Shadient/config-mint/main/fonts/hack-font
 wget -i hack-fonts.txt
 rm hack-fonts.txt
 cd ~
+
+## TERMINAL EMULATOR
+  ## install
+  git clone https://github.com/alacritty/alacritty.git
+  cd alacritty
+  cargo build --release
+  ## terminfo
+  sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+  ## Desktop entry
+  sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+  sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+  sudo desktop-file-install extra/linux/Alacritty.desktop
+  sudo update-desktop-database
+  ## Manual Page
+  sudo mkdir -p /usr/local/share/man/man1
+  gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+  gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+  cd ~
+  ## Config file
+  mkdir ~/.config/alacritty
+  wget -P ~/.config/alacritty https://raw.githubusercontent.com/Shadient/config-mint/main/alacritty/alacritty.yml
+  
+## NEOVIM
+  ## install
+  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  sudo mv nvim.appimage /usr/local/bin/nvim
+  mkdir .config/nvim
+  ## nvchad
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+  ## custom
+  cd .config/nvim
+  mkdir lua/custom
+  cp examples/init.lua lua/custom/init.lua
+  cp examples/chadrc.lua lua/custom/chadrc.lua
+  curl https://raw.githubusercontent.com/Shadient/config-mint/main/neovim/init.lua > lua/custom/init.lua
+  cd ~
